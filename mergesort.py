774e1354d566ff1068cd2a14a57d670a16911613
Samira-Kaline <36956493+Samira-Kaline@users.noreply.github.com>
2022-11-05 18:32:32 -0300

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,38 +1,41 @@
 from multiprocessing import shared_memory
 import os
 
-def merge(vetor,p,meio,u):
-  L = vetor[p:meio+1]
-  R = vetor[meio+1:u+1]
+
+def merge(vetor, p, meio, u):
+  L = []
+  R = []
+  for i in range(p, meio + 1):
+    L.append(vetor[i])
+  for i in range(meio + 1, u + 1):
+    R.append(vetor[i])
   k = p
-  while(len(L)>0 and len(R)>0):
-    if(int(L[0])<int(R[0])):
+  while (len(L) > 0 and len(R) > 0):
+    if (int(L[0]) < int(R[0])):
       vetor[k] = L.pop(0)
     else:
       vetor[k] = R.pop(0)
-    k+=1
-  while(len(L)>0):
+    k += 1
+  while (len(L) > 0):
     vetor[k] = L.pop(0)
-    k+=1
-  while(len(R)>0):
+    k += 1
+  while (len(R) > 0):
     vetor[k] = R.pop(0)
-    k+=1
-  
-def mergeSort(vetor,p,u):
-  if(p<u):
-    meio = int((p+u)/2)
-    if(os.fork()==0):
-      mergeSort(vetor,p,meio)
-      exit(0)
-    if(os.fork()!=0):
-      mergeSort(vetor,meio+1,u)
-      exit(0)
-    merge(vetor,p,meio,u)
+    k += 1
 
-print("Informe um vetor de numeros",end=" ")
-vetor = list(map(int,input().split()))
+
+def mergeSort(vetor, p, u):
+  if (p < u):
+    meio = int((p + u) // 2)
+    mergeSort(vetor, p, meio)
+    mergeSort(vetor, meio + 1, u)
+    if (os.fork() != 0):
+      merge(vetor, p, meio, u)
+      exit(0)
+
+
+print("Informe um vetor de numeros", end=" ")
+vetor = list(map(int, input().split()))
 vetor = shared_memory.ShareableList(vetor)
-mergeSort(vetor,0,len(vetor)-1)
-os.wait()
+mergeSort(vetor, 0, len(vetor) - 1)
 print(vetor)
-vetor.shm.unlink()
